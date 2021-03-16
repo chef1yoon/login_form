a7ad336f39e8d9d8bd0fc0a08a9489826f794455
chef1yoon <chef1yoon@naver.com>
2021-03-16 11:45:10 +0900

    브라우저를 이용한 라우팅 (기본화면 & login안내 화면 만들기)

--- app.js
+++ app.js
@@ -1,6 +1,14 @@
 const express = require("express");
 const app = express();
 
+app.get("/", (req, res) => {
+  res.send("여기는 루트이다.");
+});
+
+app.get("/login", (req, res) => {
+  res.send("여기는 로그인 화면이다.");
+});
+
 app.listen(3000, function () {
   console.log("서버 가동");
 });
