fd8e4be1ba1fe37fd651293d27293a82b2c9a929
chef1yoon <chef1yoon@naver.com>
2021-03-16 18:18:26 +0900

    fetch 프론트에서 서버로 데이터 보내기

--- app/src/public/js/home/login.js
+++ app/src/public/js/home/login.js
@@ -14,5 +14,11 @@ function login() {
     psword: psword.value,
   };
 
-  console.log(req);
+  fetch("/login", {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json",
+    },
+    body: JSON.stringify(req),
+  });
 }
