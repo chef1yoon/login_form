88b7a1e17644fbe9e0bf4ed3803945f04d91ffc4
chef1yoon <chef1yoon@naver.com>
2021-03-16 23:46:31 +0900

    로그인 인증 기능 만들기 in 서버

--- app/src/routes/home/home.ctrl.js
+++ app/src/routes/home/home.ctrl.js
@@ -10,9 +10,30 @@ const output = {
   },
 };
 
+const users = {
+  name: ["karen", "coco", "hello"],
+  email: ["karen@email.com", "coco@email.com", "hello@email.com"],
+  psword: ["123456", "12345678", "123456789"],
+};
+
 const process = {
   login: (req, res) => {
-    console.log(req.body);
+    const name = req.body.name,
+      email = req.body.email,
+      psword = req.body.psword;
+
+    if (users.name.includes(name)) {
+      const namex = users.name.indexOf(name);
+      if (users.pswrod[namex] === psword) {
+        return res.json({
+          success: true,
+        });
+      }
+    }
+    return res.json({
+      success: false,
+      msg: "로그인에 실패",
+    });
   },
 };
 
