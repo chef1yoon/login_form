44adf037c4683a54f7213f97e2c332e365302021
chef1yoon <chef1yoon@naver.com>
2021-03-16 13:50:12 +0900

    라우팅 분리 및 미들웨어 등록 (feat. PORT번호 변수로 설정)

--- app.js
+++ app.js
@@ -1,19 +1,21 @@
+"use strict";
+
+//모듈
 const express = require("express");
 const app = express();
+// 라우팅 (home이 routes/home파일 안에 있는 내용을 연결시키기 위해 선언해주었다.)
+const home = require("./routes/home");
+//포트번호를 변수로 받아서 설정해주는것이 좋다.
+const PORT = 3000;
 
 //앱 세팅
 app.set("views", "./views");
 //ejs 엔진으로 코드를 해석하겠다.
 app.set("view engine", "ejs");
 
-app.get("/", (req, res) => {
-  res.render("home/index");
-});
+//미들웨어를 등록하는 메소드로 index.js를 연결시켜준다.
+app.use("/", home);
 
-app.get("/login", (req, res) => {
-  res.render("home/login");
-});
-
-app.listen(3000, function () {
+app.listen(PORT, function () {
   console.log("서버 가동");
 });
