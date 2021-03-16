6e4794a9be951b8b317853d59176ff2b9b68f9b0
chef1yoon <chef1yoon@naver.com>
2021-03-16 15:44:27 +0900

    프런트를 위한 javascript 파일 만들기 (public 폴더 연결)

--- app/app.js
+++ app/app.js
@@ -10,6 +10,9 @@ const home = require("./src/routes/home");
 app.set("views", "./src/views");
 //ejs 엔진으로 코드를 해석하겠다.
 app.set("view engine", "ejs");
+//파일경로를 쉽게 찾을수 있도록 세팅(__dirname은 현재 app.js폴더 위치를 말한다.)
+// src/public경로를 정적으로 하겠다라는 뜻이다.
+app.use(express.static(`${__dirname}/src/public`));
 
 //미들웨어를 등록하는 메소드로 index.js를 연결시켜준다.
 app.use("/", home);
